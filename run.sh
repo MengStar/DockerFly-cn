4b1b1fcc04d7b3491d590978a3e444760b23e5f8
MengStar <641510128@qq.com>
2019-03-12 20:10:07 +0800

    修改菜单

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name dockerfly -d   -v /var/run/docker.sock:/var/run/docker.sock   --restart always  -p 28083:28083  dockerfly
+docker run --name dockerfly -d   -v /var/run/docker.sock:/var/run/docker.sock  --privileged --restart always  -p 28083:28083  dockerfly
