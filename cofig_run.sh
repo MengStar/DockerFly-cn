581d6812cd4d52ba7feddd02326fe47aab6513b4
MengStar <641510128@qq.com>
2019-03-13 14:21:53 +0800

    config 启动脚本

--- cofig_run.sh
+++ cofig_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker network create -d overlay arcms
+docker network create -d bridge arcms
 docker run  --network arcms -p 8080:8080 registry.cn-hangzhou.aliyuncs.com/mengstars/sync
 docker run  --network arcms -p 8090:8090 registry.cn-hangzhou.aliyuncs.com/mengstars/admin
 docker run  --network arcms -p 8070:8070 registry.cn-hangzhou.aliyuncs.com/mengstars/portal
