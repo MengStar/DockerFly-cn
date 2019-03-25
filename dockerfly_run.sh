b6756c59fc98569600d9c068c5c7754926e3ec57
mengstar <641510128@qq.com>
2019-03-25 16:29:16 +0800

    修改脚本

--- dockerfly_run.sh
+++
@@ -1,1 +0,0 @@
-docker run --name dockerfly -d   -v /var/run/docker.sock:/var/run/docker.sock  --privileged --restart always  -p 28083:28083  registry.cn-hangzhou.aliyuncs.com/mengstars/dockerfly
---
+++ script/dockerfly_run.sh
@@ -0,0 +1,2 @@
+docker pull registry.cn-hangzhou.aliyuncs.com/mengstars/dockerfly 
+docker stack deploy -c dockerfly-deploy.yml dockerManager
