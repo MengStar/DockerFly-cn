b770533e8a66696933d5de60805ad896667c1a16
mengstar <641510128@qq.com>
2019-04-06 23:04:13 +0800

    修改脚本

--- script/cofig_run.sh
+++ script/cofig_run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker network create -d bridge arcms
-docker run  --name sync --network arcms -p 8080:8080 registry.cn-hangzhou.aliyuncs.com/mengstars/sync
-docker run  --name admin --network arcms -p 8090:8090 registry.cn-hangzhou.aliyuncs.com/mengstars/admin
-docker run  --name portal --network arcms -p 8070:8070 registry.cn-hangzhou.aliyuncs.com/mengstars/portal
+# docker network create -d bridge arcms
+# docker run  --name sync --network arcms -p 8080:8080 registry.cn-hangzhou.aliyuncs.com/mengstars/sync
+# docker run  --name admin --network arcms -p 8090:8090 registry.cn-hangzhou.aliyuncs.com/mengstars/admin
+# docker run  --name portal --network arcms -p 8070:8070 registry.cn-hangzhou.aliyuncs.com/mengstars/portal
 
-#docker stack deploy -c config_deploy.yml arcms
+docker stack deploy -c config_deploy.yml arcms
