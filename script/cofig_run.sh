638c96470d81c4166677a639f746ede83e4a6fbc
mengstar <641510128@qq.com>
2019-04-07 21:23:01 +0800

    1

--- script/cofig_run.sh
+++ script/cofig_run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 docker network create -d bridge arcms
-docker run  --name syncv1 -d --network arcms -p 8080:8080 registry.cn-hangzhou.aliyuncs.com/mengstars/syncv1
-docker run  --name adminv1 -d --network arcms -p 8090:8090 registry.cn-hangzhou.aliyuncs.com/mengstars/adminv1
-docker run  --name portalv1 -d --network arcms -p 8070:8070 registry.cn-hangzhou.aliyuncs.com/mengstars/portalv1
+docker run  --name sync -d --network arcms -p 8080:8080 registry.cn-hangzhou.aliyuncs.com/mengstars/syncv1
+docker run  --name admin -d --network arcms -p 8090:8090 registry.cn-hangzhou.aliyuncs.com/mengstars/adminv1
+docker run  --name portal -d --network arcms -p 8070:8070 registry.cn-hangzhou.aliyuncs.com/mengstars/portalv1
 
 
 # docker stack deploy -c config_fake_deploy.yml arcms
